9ec508c0253b0539041283e2365f9543a26f5b8b
Mathis Burger <handymathis@gmail.com>
2026-04-14 23:47:50 +0200

    fix: Also added ID to list topic elements

--- mynd-backend/src/main/java/de/thi/mynd/topic/dto/ListTopicDto.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/dto/ListTopicDto.java
@@ -4,10 +4,13 @@ import de.thi.mynd.topic.entity.Category;
 import de.thi.mynd.topic.entity.Topic;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import lombok.Builder;
 
 @Builder
 public final class ListTopicDto {
+
+  public UUID id;
 
   public String title;
 
@@ -17,6 +20,7 @@ public final class ListTopicDto {
 
   public static ListTopicDto from(Topic topic) {
     return ListTopicDto.builder()
+        .id(topic.id)
         .title(topic.title)
         .categories(topic.categories)
         .updatedAt(topic.updatedAt)
