85dc9dce4420be50ecb9ad3983f61b1dc58ced2c
Mathis Burger <handymathis@gmail.com>
2026-04-15 15:18:01 +0200

    feat: Adjusted color in category select

--- frontend/src/components/topic/category-select.tsx
+++ frontend/src/components/topic/category-select.tsx
@@ -1,27 +1,26 @@
 import { useMemo, useState } from 'react';
 import type { Category } from '../../schemas/topic.ts';
 import { useDebouncedValue } from '@mantine/hooks';
-import { Loader, MultiSelect } from '@mantine/core';
+import { Loader, MultiSelect, type MultiSelectProps, Pill } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
 import { useQueryCategories } from '../../api/topic.ts';
 import type { GetInputPropsReturnType } from '@mantine/form';
+import { getContrastColor } from '../../utils/color.ts';
 
 const CategorySelect = ({onChange, value, error, onFocus, onBlur}: GetInputPropsReturnType) => {
   const { t } = useTranslation();
+
+  const selectedCategories = (value ?? []) as Category[];
 
   const [searchValue, setSearchValue] = useState('');
   const [debouncedSearch] = useDebouncedValue(searchValue, 300);
   const { data: searchResults, isLoading, isFetching } = useQueryCategories(debouncedSearch);
 
-  console.log(searchResults);
-
-  const [selectedCategoriesBuffer, setSelectedCategoriesBuffer] = useState<Category[]>([]);
-
   const selectableData = useMemo(() => {
     const results = searchResults ?? [];
     const combined = [...results];
 
-    selectedCategoriesBuffer.forEach((item) => {
+    selectedCategories.forEach((item) => {
       if (!combined.some((c) => c.id === item.id)) {
         combined.push(item);
       }
@@ -31,17 +30,36 @@ const CategorySelect = ({onChange, value, error, onFocus, onBlur}: GetInputProps
       value: cat.id,
       label: cat.title,
     }));
-  }, [searchResults, selectedCategoriesBuffer]);
+  }, [searchResults, selectedCategories]);
 
   const handleChange = (ids: string[]) => {
-    const allKnownCategories = [...(searchResults ?? []), ...selectedCategoriesBuffer];
+    const allKnownCategories = [...(searchResults ?? []), ...selectedCategories];
 
     const newlySelectedObjects = ids
       .map((id) => allKnownCategories.find((c) => c.id === id))
       .filter((c): c is Category => !!c);
 
     onChange(newlySelectedObjects);
-    setSelectedCategoriesBuffer(newlySelectedObjects);
+  };
+
+  const renderMultiSelectPill: MultiSelectProps['renderPill'] = (props) => {
+    const category = selectedCategories.find((c) => c.id === props.value);
+
+    console.log(category);
+    return (
+      <Pill
+        withRemoveButton
+        onRemove={props.onRemove}
+        styles={{
+          root: {
+            backgroundColor: category?.color ? `#${category.color}` : undefined,
+            color: category?.color ? getContrastColor(category.color) : undefined
+        }}}
+        variant="filled"
+      >
+        {props.option.label}
+      </Pill>
+    );
   };
 
   return (
@@ -63,6 +81,7 @@ const CategorySelect = ({onChange, value, error, onFocus, onBlur}: GetInputProps
       onBlur={onBlur}
       onFocus={onFocus}
       withAsterisk
+      renderPill={renderMultiSelectPill}
     />
   );
 };
