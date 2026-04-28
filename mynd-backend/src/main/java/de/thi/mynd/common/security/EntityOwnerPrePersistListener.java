f03154f70a696cc29eb80fce1dfd3edcada92d50
Mathis Burger <handymathis@gmail.com>
2026-04-28 21:27:26 +0200

    fix: Fixed content element creation

--- mynd-backend/src/main/java/de/thi/mynd/common/security/EntityOwnerPrePersistListener.java
+++ mynd-backend/src/main/java/de/thi/mynd/common/security/EntityOwnerPrePersistListener.java
@@ -4,12 +4,14 @@ import de.thi.mynd.common.entity.BaseEntity;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.PrePersist;
 
 @ApplicationScoped
 public final class EntityOwnerPrePersistListener {
 
   @Inject SecurityIdentity securityIdentity;
 
+  @PrePersist
   public void setOwner(Object entity) {
     if (entity instanceof BaseEntity baseEntity) {
       if (baseEntity.creatorId == null && !securityIdentity.isAnonymous()) {
