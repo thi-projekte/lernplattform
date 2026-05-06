00ce4e6d5116398210f10d2f797289288ad7295e
Mathis Burger <handymathis@gmail.com>
2026-05-06 16:55:38 +0200

    feat: Added service and mapping layer for topic graph functionality

---
+++ mynd-backend/src/main/java/de/thi/mynd/topic/processor/mapper/GraphTopicDtoMapper.java
@@ -0,0 +1,57 @@
+package de.thi.mynd.topic.processor.mapper;
+
+import de.thi.mynd.common.processor.AbstractMappingProcessor;
+import de.thi.mynd.common.service.IdentityService;
+import de.thi.mynd.topic.dto.graph.GraphTopicDto;
+import de.thi.mynd.topic.entity.Topic;
+import de.thi.mynd.topic.entity.TopicAssociation;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public final class GraphTopicDtoMapper extends AbstractMappingProcessor<Topic, GraphTopicDto> {
+
+    @Inject
+    IdentityService identityService;
+
+    @Override
+    public GraphTopicDto mapAndEnrich(Topic entity) {
+
+        List<UUID> associatedTopicIds = new ArrayList<>();
+        associatedTopicIds.addAll(
+                entity.ownedAssociations.stream()
+                        .map(a -> a.foreignTopic.id)
+                        .toList()
+        );
+        associatedTopicIds.addAll(
+                entity.foreignAssociations.stream()
+                        .map(a -> a.owningTopic.id)
+                        .toList()
+        );
+
+
+        return GraphTopicDto.builder()
+                .id(entity.id)
+                .title(entity.title)
+                .categories(entity.categories)
+                .updatedAt(entity.updatedAt)
+                .creatorId(entity.creatorId)
+                .creatorFullName(identityService.getFullNameByUsername(entity.creatorId))
+                .associatedTopics(associatedTopicIds)
+                .build();
+    }
+
+    @Override
+    public Class<Topic> getEntityType() {
+        return Topic.class;
+    }
+
+    @Override
+    public Class<GraphTopicDto> getDtoType() {
+        return GraphTopicDto.class;
+    }
+}
---
+++ mynd-backend/src/main/java/de/thi/mynd/topic/service/TopicGraphServiceImpl.java
@@ -0,0 +1,43 @@
+package de.thi.mynd.topic.service;
+
+import de.thi.mynd.common.processor.MappingRegistry;
+import de.thi.mynd.topic.dto.graph.GraphTopicDto;
+import de.thi.mynd.topic.entity.Topic;
+import de.thi.mynd.topic.repository.TopicGraphRepository;
+import de.thi.mynd.topic.repository.TopicRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public final class TopicGraphServiceImpl implements TopicGraphService {
+
+    @Inject
+    TopicGraphRepository topicRepository;
+
+    @Inject
+    MappingRegistry mappingRegistry;
+
+    @Override
+    public List<GraphTopicDto> getNMostPopularTopicsInGraphAndTheirDirectNeighbors(int n) {
+        List<Topic> topics = topicRepository.findNMostPopular(n);
+
+        return mappingRegistry.mapList(topics, GraphTopicDto.class);
+    }
+
+    @Override
+    public List<GraphTopicDto> getNMostPopularTopicsInGraphAndTheirDirectNeighbors(int n, List<UUID> categoryFilter) {
+        List<Topic> topics = topicRepository.findNMostPopularFilterByCategoryIds(n, categoryFilter);
+
+        return mappingRegistry.mapList(topics, GraphTopicDto.class);
+    }
+
+    @Override
+    public List<GraphTopicDto> getNeighborsOfTopic(UUID topicId) {
+        List<Topic> topics = topicRepository.findNeighborsByTopicId(topicId);
+
+        return mappingRegistry.mapList(topics, GraphTopicDto.class);
+    }
+}
