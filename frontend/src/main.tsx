30f2f801621ff6c66c9d6532afd016de8c9e7150
Mathis Burger <handymathis@gmail.com>
2026-05-06 09:25:35 +0200

    feat: Added custom templates to keycloak

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,26 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import keycloak from './keycloak.ts';
+import  { type KcContext, KcPage } from './keycloak-theme/kc.gen.tsx';
 
-keycloak
-  .init({
-    onLoad: 'login-required',
-    // Needs to enabled in order to work with vite HMR
-    checkLoginIframe: false,
-  })
-  .then((authenticated) => {
-    if (authenticated) {
-      createRoot(document.getElementById('root')!).render(<App />);
-    }
-  })
-  .catch((err) => {
-    console.error('Keycloak init failed', err);
-  });
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const kcContext = (window as any).kcContext as KcContext | undefined;
+
+if (kcContext) {
+  createRoot(document.getElementById('root')!).render(<KcPage kcContext={kcContext} />);
+} else {
+  keycloak
+    .init({
+      onLoad: 'login-required',
+      // Needs to enabled in order to work with vite HMR
+      checkLoginIframe: false,
+    })
+    .then((authenticated) => {
+      if (authenticated) {
+        createRoot(document.getElementById('root')!).render(<App />);
+      }
+    })
+    .catch((err) => {
+      console.error('Keycloak init failed', err);
+    });
+}
