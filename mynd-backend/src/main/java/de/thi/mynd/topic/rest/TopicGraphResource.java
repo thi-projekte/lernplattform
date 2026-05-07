c2ae80248316093a8cf514aaa0c1313c3a9a2fec
Mathis Burger <handymathis@gmail.com>
2026-05-07 15:35:47 +0200

    fix: Explicitly stated method

--- mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicGraphResource.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicGraphResource.java
@@ -3,6 +3,7 @@ package de.thi.mynd.topic.rest;
 import de.thi.mynd.topic.dto.graph.GraphTopicDto;
 import de.thi.mynd.topic.service.TopicGraphService;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +15,7 @@ public final class TopicGraphResource {
   @Inject TopicGraphService topicGraphService;
 
   @Path("/most-popular")
+  @GET
   public List<GraphTopicDto> getMostPopular(@RestQuery List<UUID> categories) {
     int n = 10;
     if (categories == null) {
@@ -23,6 +25,7 @@ public final class TopicGraphResource {
   }
 
   @Path("/{topicId}/graph-neighbors")
+  @GET
   public List<GraphTopicDto> getNeighbors(UUID topicId) {
     return topicGraphService.getNeighborsOfTopic(topicId);
   }
