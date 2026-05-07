933e9f272d6af20cb854872cf434109e27af2c25
Anna <fender23@MacBook-Air-Potato.local>
2026-05-08 01:29:50 +0200

    style: update and align app logo

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -5,7 +5,6 @@ import { type FC, type ReactNode, useMemo, useState } from 'react';
 import LanguagePicker from './language-picker.tsx';
 import { useTranslation } from 'react-i18next';
 
-import logo from '../assets/logo.png';
 import keycloak from '../keycloak.ts';
 import { routes, type TypedMyndRoute } from '../routing.ts';
 import { useLocation, useMatches, useNavigate } from 'react-router';
@@ -50,7 +49,7 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <AppShell
-      header={{ height: 60 }}
+      header={{ height: 104 }}
       navbar={{
         width: desktopNavbarWidth,
         breakpoint: 'sm',
@@ -59,13 +58,30 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
       padding={0}
     >
       <AppShell.Header>
-        <Group h="100%" px="md" justify="space-between">
-          <Group>
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <Image src={logo} alt="MYnd Logo" h={45} w="auto" fit="contain" />
+        <Group h="100%" justify="space-between" wrap="nowrap">
+          <Box
+            visibleFrom="sm"
+            style={{
+              width: desktopNavbarWidth,
+              minWidth: desktopNavbarWidth,
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingLeft: 35,
+              overflow: 'visible',
+              transition: 'width 150ms ease',
+            }}
+          >
+            <Image src="/mynd-logo.png" alt="MYnd Logo" w={180} h="auto" fit="contain" />
+          </Box>
+
+          <Group hiddenFrom="sm" gap="sm" h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} size="sm" />
+            <Image src="/mynd-logo.png" alt="MYnd Logo" h={58} w="auto" fit="contain" />
           </Group>
 
-          <Group>
+          <Group px="md">
             <LanguagePicker />
             <ActionIcon variant="default" size="xl" onClick={() => keycloak.accountManagement()}>
               <IconUser size={32} stroke={1.5} />
