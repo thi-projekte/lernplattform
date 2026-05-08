51960efab978193be078b287d62b44c018dda5a4
Mathis Burger <handymathis@gmail.com>
2026-05-08 14:10:41 +0200

    feat: Updated topic graph resource

--- mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicGraphResource.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicGraphResource.java
@@ -3,6 +3,7 @@ package de.thi.mynd.topic.rest;
 import de.thi.mynd.topic.dto.graph.GraphTopicDto;
 import de.thi.mynd.topic.service.TopicGraphService;
 import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -15,20 +16,34 @@ import org.jboss.resteasy.reactive.RestQuery;
 public final class TopicGraphResource {
 
   @Inject TopicGraphService topicGraphService;
+  @Inject SecurityIdentity securityIdentity;
 
   @Path("/most-popular")
   @GET
-  public List<GraphTopicDto> getMostPopular(@RestQuery List<UUID> categories) {
-    int n = 10;
+  public List<GraphTopicDto> getMostPopular(
+      @RestQuery List<UUID> categories, @RestQuery Boolean personal) {
     if (categories.isEmpty()) {
-      return topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(n);
+      return personal
+          ? topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(
+              100, securityIdentity.getPrincipal().getName())
+          : topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(10);
     }
-    return topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(n, categories);
+    return personal
+        ? topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(
+            100, categories, securityIdentity.getPrincipal().getName())
+        : topicGraphService.getNMostPopularTopicsInGraphAndTheirDirectNeighbors(10, categories);
   }
 
   @Path("/{topicId}/graph-neighbors")
   @GET
-  public List<GraphTopicDto> getNeighbors(UUID topicId) {
-    return topicGraphService.getNeighborsOfTopic(topicId);
+  public List<GraphTopicDto> getNeighbors(UUID topicId, @RestQuery Boolean personal) {
+    return personal
+        ? topicGraphService.getOwnedNeighborsOfTopic(topicId)
+        : topicGraphService.getNeighborsOfTopic(topicId);
+  }
+
+  @GET
+  public List<GraphTopicDto> searchTopic(@RestQuery String search) {
+    return topicGraphService.searchTopicNodes(search, 5);
   }
 }
