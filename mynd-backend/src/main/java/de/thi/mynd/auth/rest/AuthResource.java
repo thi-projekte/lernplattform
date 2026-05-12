b725f525236eb86a6431fddf7bfdfc6eaa2dba91
Mathis Burger <handymathis@gmail.com>
2026-05-13 00:16:02 +0200

    feat: Added proper endpoint documentation in openAPI for Auth resource

--- mynd-backend/src/main/java/de/thi/mynd/auth/rest/AuthResource.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/rest/AuthResource.java
@@ -8,8 +8,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/auth")
+@Tag(name = "Authorization")
 public final class AuthResource {
 
   @Inject AuthService authService;
@@ -19,6 +23,9 @@ public final class AuthResource {
   @GET
   @Path("/check-is-builder")
   @Authenticated
+  @Operation(summary = "Check if the user is builder", description = "Checks whether the current user has the builder role assigned.")
+  @APIResponse(responseCode = "200", description = "The current user has the builder role")
+  @APIResponse(responseCode = "204", description = "The current user does not have the builder role")
   public Response checkUserIsBuilder() {
     String username = identity.getPrincipal().getName();
     if (authService.checkUserIsBuilder(username)) {
@@ -30,6 +37,9 @@ public final class AuthResource {
   @POST
   @Path("/register-as-builder")
   @Authenticated
+  @Operation(summary = "Register the current user as builder", description = "Assigns the current user the builder role")
+  @APIResponse(responseCode = "201", description = "The role got assigned successfully")
+  @APIResponse(responseCode = "404", description = "The user could not be found in keycloak")
   public Response makeUserBuilder() {
     String username = identity.getPrincipal().getName();
     authService.makeUserABuilder(username);
@@ -39,6 +49,9 @@ public final class AuthResource {
   @POST
   @Path("/register-as-learner")
   @Authenticated
+  @Operation(summary = "Register the current user as learner", description = "Assigns the current user the learner role")
+  @APIResponse(responseCode = "201", description = "The role got assigned successfully")
+  @APIResponse(responseCode = "404", description = "The user could not be found in keycloak")
   public Response makeUserLearner() {
     String username = identity.getPrincipal().getName();
     authService.makeUserALearner(username);
