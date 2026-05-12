f4b4c2196c381143618916253b452deb6d024ccd
Mathis Burger <handymathis@gmail.com>
2026-05-13 01:14:22 +0200

    feat: Added better open api doc for topic resource

--- mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicResource.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/rest/TopicResource.java
@@ -1,7 +1,6 @@
 package de.thi.mynd.topic.rest;
 
 import de.thi.mynd.common.dto.PaginationDto;
-import de.thi.mynd.common.exception.EntityInstanceNotFoundException;
 import de.thi.mynd.topic.dto.ListTopicDto;
 import de.thi.mynd.topic.dto.TopicDto;
 import de.thi.mynd.topic.requests.TopicRequest;
@@ -15,11 +14,19 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 import java.util.UUID;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/topics")
 @Produces(MediaType.APPLICATION_JSON)
 @Authenticated
+@Tag(name = "Topics")
+@SecurityRequirement(name = "keycloak")
 public final class TopicResource {
 
   @Inject TopicService topicService;
@@ -27,6 +34,9 @@ public final class TopicResource {
   @GET
   @Path("/personal")
   @RolesAllowed("builder")
+  @Operation(
+          summary = "Gets all the personal topics of the logged in builder",
+          description = "Gets all the personal topics of the logged in builder in a paginated form")
   public PaginationDto<ListTopicDto> getPersonalTopicsPaginated(
       @RestQuery int page, @RestQuery int pageSize) {
     return topicService.findPersonalTopicsPaginated(page, pageSize);
@@ -34,21 +44,30 @@ public final class TopicResource {
 
   @GET
   @Path("/{topicId}")
-  public TopicDto getTopic(UUID topicId, @RestQuery boolean withOwnedRelatedTopics) {
-    try {
+  @Operation(
+          summary = "Finds a topic by topicId",
+          description = "Finds a topic that is associated with the given topicId")
+  @APIResponse(responseCode = "404", description = "The topic does not exist")
+  public TopicDto getTopic(UUID topicId, @Parameter(description = "Indicates whether all the related topics that the topic owns should be provided too") @RestQuery boolean withOwnedRelatedTopics) {
       return topicService.getTopic(topicId, withOwnedRelatedTopics);
-    } catch (EntityInstanceNotFoundException e) {
-      throw new NotFoundException(e);
-    }
   }
 
   @GET
-  public List<ListTopicDto> search(@RestQuery String search) {
+  @Operation(
+          summary = "Searches for topics",
+          description = "Searches a maximum of five topics based on the search string.")
+  public List<ListTopicDto> search(@Parameter(
+          description = "The search string that is used to find specific topics",
+          required = true,
+          example = "KI")@RestQuery String search) {
     return topicService.findTopicsBySearchMax5(search);
   }
 
   @POST
   @RolesAllowed("builder")
+  @Operation(
+          summary = "Creates a new topic",
+          description = "Creates a new topic and assigns the provided content elements to it.")
   public TopicDto createTopic(@Valid TopicRequest createTopicRequest) {
     return topicService.createTopic(createTopicRequest);
   }
@@ -56,23 +75,23 @@ public final class TopicResource {
   @PUT
   @Path("/{topicId}")
   @RolesAllowed("builder")
+  @Operation(
+          summary = "Updates a topic",
+          description = "Updates a topic and updates the provided content elements by ID.")
+  @APIResponse(responseCode = "404", description = "The topic does not exist")
   public TopicDto updateTopic(UUID topicId, @Valid TopicRequest updateTopicRequest) {
-    try {
       return topicService.updateTopic(topicId, updateTopicRequest);
-    } catch (EntityInstanceNotFoundException e) {
-      throw new NotFoundException(e);
-    }
   }
 
   @DELETE
   @Path("/{topicId}")
   @RolesAllowed("builder")
+  @Operation(
+          summary = "Deletes a topic",
+          description = "Deletes the topic with the given ID")
+  @APIResponse(responseCode = "404", description = "The topic does not exist")
   public Response deleteTopic(UUID topicId) {
-    try {
       topicService.deleteTopic(topicId);
       return Response.ok().build();
-    } catch (EntityInstanceNotFoundException e) {
-      throw new NotFoundException(e.getMessage());
-    }
   }
 }
