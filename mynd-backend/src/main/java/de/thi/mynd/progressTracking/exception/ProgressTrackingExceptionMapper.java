a8cd8ef8b1bd343085a14d133c1f00bab95763ee
Mathis Burger <handymathis@gmail.com>
2026-05-14 00:09:20 +0200

    feat: Added exception mapper for progress tracking

---
+++ mynd-backend/src/main/java/de/thi/mynd/progressTracking/exception/ProgressTrackingExceptionMapper.java
@@ -0,0 +1,12 @@
+package de.thi.mynd.progressTracking.exception;
+
+import jakarta.ws.rs.core.Response;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+public final class ProgressTrackingExceptionMapper {
+
+    @ServerExceptionMapper
+    public Response mapTopicLearnProgressNotStarted(TopicLearnProgressNotStartedException e) {
+        return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+    }
+}
---
+++ mynd-backend/src/main/java/de/thi/mynd/progressTracking/exception/TopicLearnProgressNotStartedException.java
@@ -0,0 +1,7 @@
+package de.thi.mynd.progressTracking.exception;
+
+public class TopicLearnProgressNotStartedException extends RuntimeException {
+    public TopicLearnProgressNotStartedException(String message) {
+        super(message);
+    }
+}
