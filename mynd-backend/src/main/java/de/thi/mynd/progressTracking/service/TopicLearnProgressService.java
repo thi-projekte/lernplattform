f4d07fdde651f111fafcc9608fe076d2840892f9
Mathis Burger <handymathis@gmail.com>
2026-05-13 23:46:17 +0200

    refactor: Renamed interface to express more meaning

--- mynd-backend/src/main/java/de/thi/mynd/progressTracking/service/TopicLearnProgressService.java
+++ mynd-backend/src/main/java/de/thi/mynd/progressTracking/service/TopicLearnProgressService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public interface LearnProgressService {
+public interface TopicLearnProgressService {
 
     Map<UUID, TopicLearnProgressDto> getLearnProgressMappingForTopics(List<UUID> topicIds);
 
