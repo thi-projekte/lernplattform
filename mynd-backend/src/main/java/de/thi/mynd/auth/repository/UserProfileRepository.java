a1fd7f7aff3f080cfef0834635968cd2fdad6751
Mathis Burger <handymathis@gmail.com>
2026-05-17 14:38:41 +0200

    fix: Use different repository

--- mynd-backend/src/main/java/de/thi/mynd/auth/repository/UserProfileRepository.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/repository/UserProfileRepository.java
@@ -1,12 +1,12 @@
 package de.thi.mynd.auth.repository;
 
 import de.thi.mynd.auth.entity.UserProfile;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import de.thi.mynd.common.repository.MyndBaseCustomIdRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.Optional;
 
 @ApplicationScoped
-public final class UserProfileRepository implements PanacheRepositoryBase<UserProfile, String> {
+public final class UserProfileRepository extends MyndBaseCustomIdRepository<UserProfile, String> {
 
   public Optional<UserProfile> findByUsername(String username) {
     return findByIdOptional(username);
