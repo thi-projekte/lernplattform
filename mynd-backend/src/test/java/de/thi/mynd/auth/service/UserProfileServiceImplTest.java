617f219b5a2f28cef503d3baf4dd321c0d2a178e
Mathis Burger <handymathis@gmail.com>
2026-05-17 15:09:18 +0200

    feat: Reimported exception from right place

--- mynd-backend/src/test/java/de/thi/mynd/auth/service/UserProfileServiceImplTest.java
+++ mynd-backend/src/test/java/de/thi/mynd/auth/service/UserProfileServiceImplTest.java
@@ -5,11 +5,11 @@ import static org.mockito.Mockito.*;
 
 import de.thi.mynd.auth.dto.ProfilePictureDto;
 import de.thi.mynd.auth.entity.UserProfile;
+import de.thi.mynd.auth.exception.ProfilePictureNotFoundException;
 import de.thi.mynd.auth.repository.UserProfileRepository;
 import de.thi.mynd.common.exception.FileTooLargeException;
 import de.thi.mynd.common.exception.InvalidFileTypeException;
 import de.thi.mynd.common.exception.NoFileProvidedException;
-import de.thi.mynd.common.exception.ProfilePictureNotFoundException;
 import de.thi.mynd.common.service.ObjectStorageService;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
