a2f955d4892d9a7a842df9711a5d20573fd57c04
Mathis Burger <handymathis@gmail.com>
2026-05-17 15:07:39 +0200

    chore: Fixed backend code formatting

--- mynd-backend/src/main/java/de/thi/mynd/auth/exception/AuthExceptionMapper.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/exception/AuthExceptionMapper.java
@@ -5,8 +5,8 @@ import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 public final class AuthExceptionMapper {
 
-    @ServerExceptionMapper
-    public Response mapProfilePictureNotFoundException(ProfilePictureNotFoundException e) {
-        return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
-    }
+  @ServerExceptionMapper
+  public Response mapProfilePictureNotFoundException(ProfilePictureNotFoundException e) {
+    return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+  }
 }
