81f0f39222af0751b643edf0688d9bcffcbfdbec
Mathis Burger <handymathis@gmail.com>
2026-05-17 14:38:12 +0200

    feat: Updated entity mapping for user profile

--- mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
@@ -1,26 +1,21 @@
 package de.thi.mynd.auth.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import de.thi.mynd.common.entity.BaseEntity;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "user_profile")
-public class UserProfile {
+@AttributeOverride(
+        name = "creatorId",
+        column = @Column(name = "creatorId", insertable = false, updatable = false))
+public class UserProfile extends BaseEntity {
 
-  @Id public String creatorId;
-
-  @CreationTimestamp
-  @Column(updatable = false, nullable = false)
-  public LocalDateTime createdAt;
-
-  @UpdateTimestamp
-  @Column(nullable = false)
-  public LocalDateTime updatedAt;
+  @Id
+  public String creatorId;
 
   public String profilePictureKey;
 }
