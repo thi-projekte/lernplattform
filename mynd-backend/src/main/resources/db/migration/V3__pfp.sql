29dd3d4b62a99c63ea8a26b6fb0e90fe648f1148
Mathis Burger <handymathis@gmail.com>
2026-05-17 17:20:21 +0200

    fix: Profile picture deletion

--- mynd-backend/src/main/resources/db/migration/V3__pfp.sql
+++ mynd-backend/src/main/resources/db/migration/V3__pfp.sql
@@ -2,5 +2,5 @@ create table user_profile (
     creatorId varchar(255) not null primary key ,
     createdAt timestamp(6) not null,
     updatedAt timestamp(6) not null,
-    profilePictureKey varchar(255) not null
+    profilePictureKey varchar(255)
 );
