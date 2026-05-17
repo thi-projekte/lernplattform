f7f65f7336c56cf88d71388c9798a5f41beabdce
Mathis Burger <57145815+MathisBurger@users.noreply.github.com>
2026-05-17 14:08:06 +0200

    Revert "Endpoints profilbild"

--- mynd-backend/src/main/java/de/thi/mynd/common/exception/ProfilePictureNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.thi.mynd.common.exception;
-
-public class ProfilePictureNotFoundException extends RuntimeException {
-  public ProfilePictureNotFoundException(String message) {
-    super(message);
-  }
-}
--- mynd-backend/src/main/resources/db/migration/V3__profile_picture.sql
+++
@@ -1,6 +0,0 @@
-create table user_profile (
-    creatorId varchar(255) primary key,
-    createdAt timestamp(6) not null,
-    updatedAt timestamp(6) not null,
-    profilePictureKey varchar(255)
-);
