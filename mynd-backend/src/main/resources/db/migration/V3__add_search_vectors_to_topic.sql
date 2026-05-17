48518512627aeae4b3c0dc55400e1b499c952379
Mathis Burger <handymathis@gmail.com>
2026-05-17 13:58:04 +0200

    fix: Added back migration script update clause

--- mynd-backend/src/main/resources/db/migration/V3__add_search_vectors_to_topic.sql
+++ mynd-backend/src/main/resources/db/migration/V3__add_search_vectors_to_topic.sql
@@ -4,3 +4,7 @@ ALTER TABLE topic
 
 CREATE INDEX idx_topic_title_search_vector  ON topic USING GIN (title_search_vector);
 CREATE INDEX idx_topic_teaser_search_vector ON topic USING GIN (teaser_search_vector);
+
+UPDATE topic
+SET title_search_vector  = to_tsvector('german', coalesce(title, '')),
+    teaser_search_vector = to_tsvector('german', coalesce(teaser, ''));
