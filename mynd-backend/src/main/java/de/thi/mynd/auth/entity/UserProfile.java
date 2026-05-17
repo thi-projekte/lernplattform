614d4598dabbf1225d37e47207737346b1f8dd59
Mathis Burger <handymathis@gmail.com>
2026-05-17 15:14:45 +0200

    feat: readded migration

--- mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
@@ -7,5 +7,6 @@ import jakarta.persistence.*;
 @Table(name = "user_profile")
 public class UserProfile extends BaseEntityWithCreatorIdPk {
 
+  @Column(nullable = false)
   public String profilePictureKey;
 }
