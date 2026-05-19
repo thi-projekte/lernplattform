6b459b21e02d0612ea2a51db8dc520593bd9f5e5
Mathis Burger <handymathis@gmail.com>
2026-05-19 22:17:55 +0200

    feat: Added plausible tracking to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import keycloak from './keycloak.ts';
 import { type KcContext, KcPage } from './keycloak-theme/kc.gen.tsx';
+import { init } from '@plausible-analytics/tracker';
+
+init({
+  domain: import.meta.env.VITE_ANALYTICS_URI,
+  endpoint: `${import.meta.env.VITE_ANALYTICS_INSTANCE}/api/event`,
+  // captureOnLocalhost: true
+});
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const kcContext = (window as any).kcContext as KcContext | undefined;
