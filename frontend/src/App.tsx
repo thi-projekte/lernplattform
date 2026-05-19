a372262a16ddbc30067e58079acd37444f2177d7
Mathis Burger <handymathis@gmail.com>
2026-05-19 22:53:30 +0200

    feat: Added tracking for roles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,10 +14,22 @@ import '@mantine/notifications/styles.css';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import RegistrationProvider from './provider/registration-provider.tsx';
+import type { KeycloakResourceAccess } from 'keycloak-js';
+import keycloak from './keycloak.ts';
+import { track } from '@plausible-analytics/tracker';
 
 const queryClient = new QueryClient();
 
 function App() {
+
+
+  const resourceAccess: KeycloakResourceAccess = keycloak.tokenParsed
+    ?.resource_access as KeycloakResourceAccess;
+  const roles = resourceAccess['mynd']?.roles;
+  for (const role of roles) {
+    track('role', {props: {role: role}});
+  }
+
   return (
     <MantineProvider theme={theme}>
       <ModalsProvider>
