50deadd7a5e4d9d2ffd1d14c023988f96939f2c7
Mathis Burger <handymathis@gmail.com>
2026-05-20 19:25:55 +0200

    chore: Fixed backend code formatting

--- mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
+++ mynd-backend/src/main/java/de/thi/mynd/auth/entity/UserProfile.java
@@ -7,8 +7,7 @@ import jakarta.persistence.*;
 @Table(name = "user_profile")
 public class UserProfile extends BaseEntityWithCreatorIdPk {
 
-  @Column
-  public String profilePictureKey;
+  @Column public String profilePictureKey;
 
   @Column(nullable = false)
   public int invitationsLeft;
