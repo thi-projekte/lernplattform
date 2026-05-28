6755452409da4df08b1cad783d21e5d85aa80830
Mathis Burger <handymathis@gmail.com>
2026-05-28 23:37:55 +0200

    feat: Added payment resource demo implementation

---
+++ mynd-backend/src/main/java/de/thi/mynd/subscription/rest/PaymentResource.java
@@ -0,0 +1,37 @@
+package de.thi.mynd.subscription.rest;
+
+import com.stripe.model.checkout.Session;
+import de.thi.mynd.subscription.dto.PaymentSessionDto;
+import de.thi.mynd.subscription.entity.SubscriptionStatus;
+import de.thi.mynd.subscription.service.PaymentService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.util.Map;
+
+@Path("/payments")
+@RolesAllowed("authorizedUser")
+@Tag(name = "Payment")
+@SecurityRequirement(name = "keycloak")
+public final class PaymentResource {
+
+    @Inject
+    PaymentService paymentService;
+
+    @POST
+    @Path("/create-checkout-session")
+    @Operation(
+            summary = "Creates a new checkout session",
+            description =
+                    "Creates a new checkout session for one of our subscriptions.")
+    public PaymentSessionDto createCheckoutSessionForSubscription(@FormParam("subscriptionStatus") SubscriptionStatus subscriptionStatus) {
+        return paymentService.getCheckoutSessionForSubscription(subscriptionStatus);
+    }
+}
