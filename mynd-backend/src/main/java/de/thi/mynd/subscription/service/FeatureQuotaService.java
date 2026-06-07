6884b59d8fa298784d50979a7148333fc4c1fdc1
Mathis Burger <handymathis@gmail.com>
2026-06-07 19:13:44 +0200

    feat: Added interface contract for feature quota

---
+++ mynd-backend/src/main/java/de/thi/mynd/subscription/service/FeatureQuotaService.java
@@ -0,0 +1,10 @@
+package de.thi.mynd.subscription.service;
+
+import de.thi.mynd.subscription.exception.FeatureQuotaHitException;
+
+public interface FeatureQuotaService {
+
+    void learnContentElement(String userId) throws FeatureQuotaHitException;
+
+    void startNewTopic(String userId) throws FeatureQuotaHitException;
+}
