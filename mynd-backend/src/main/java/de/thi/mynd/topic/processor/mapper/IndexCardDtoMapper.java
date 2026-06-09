0215a08dcdea011dfadd81e9d7a5045cd9b0daf1
Mathis Burger <handymathis@gmail.com>
2026-06-09 23:34:34 +0200

    feat: Added index card dto mapper

---
+++ mynd-backend/src/main/java/de/thi/mynd/topic/processor/mapper/IndexCardDtoMapper.java
@@ -0,0 +1,28 @@
+package de.thi.mynd.topic.processor.mapper;
+
+import de.thi.mynd.common.processor.AbstractMappingProcessor;
+import de.thi.mynd.topic.dto.IndexCardDto;
+import de.thi.mynd.topic.entity.IndexCard;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public final class IndexCardDtoMapper extends AbstractMappingProcessor<IndexCard, IndexCardDto> {
+    @Override
+    public IndexCardDto mapAndEnrich(IndexCard entity) {
+        return IndexCardDto.builder()
+                .id(entity.id)
+                .question(entity.question)
+                .answer(entity.answer)
+                .build();
+    }
+
+    @Override
+    public Class<IndexCard> getEntityType() {
+        return IndexCard.class;
+    }
+
+    @Override
+    public Class<IndexCardDto> getDtoType() {
+        return IndexCardDto.class;
+    }
+}
