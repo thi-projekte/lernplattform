35af5d2aa15ce9716da97d4e9b8a9f9c23ac669a
Mathis Burger <handymathis@gmail.com>
2026-06-09 23:45:36 +0200

    feat: Added tests for index card service

--- mynd-backend/src/main/java/de/thi/mynd/topic/request/IndexCardRequest.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/request/IndexCardRequest.java
@@ -1,9 +1,7 @@
 package de.thi.mynd.topic.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 
-@Builder
 public final class IndexCardRequest {
 
     @NotBlank public String question;
