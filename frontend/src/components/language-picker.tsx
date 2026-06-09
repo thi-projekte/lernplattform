19da3035cdf9bd72053f74b545d3c9cbcd278675
Mathis Burger <handymathis@gmail.com>
2026-06-09 20:45:25 +0200

    fix(#260): Updated language picker to work properly

--- frontend/src/components/language-picker.tsx
+++ frontend/src/components/language-picker.tsx
@@ -1,16 +1,31 @@
 import { UnstyledButton, Menu, Group, Text } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
 import { IconChevronDown, IconWorld } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
 
 const supportedLanguages = [
   { label: 'Deutsch', value: 'de', flag: '🇩🇪' },
   { label: 'English', value: 'en', flag: '🇺🇸' },
 ];
 
+const getPreferredLanguage = () => {
+  return localStorage.getItem("language");
+}
+
 const LanguagePicker = () => {
   const { i18n } = useTranslation();
+
+  const [languageSelected, setSelectedLanguage] = useState<string|null>(getPreferredLanguage() || supportedLanguages[0].value);
+
   const activeLang =
-    supportedLanguages.find((l) => l.value === i18n.language) || supportedLanguages[0];
+    supportedLanguages.find((l) => l.value === languageSelected);
+
+  useEffect(() => {
+    if (languageSelected !== null) {
+      i18n.changeLanguage(languageSelected);
+      localStorage.setItem("language", languageSelected);
+    }
+  }, [i18n, languageSelected]);
 
   return (
     <Menu shadow="md" width={150}>
@@ -19,7 +34,7 @@ const LanguagePicker = () => {
           <Group gap="xs">
             <IconWorld size={20} stroke={1.5} />
             <Text size="sm" fw={500} visibleFrom="xs">
-              {activeLang.value.toUpperCase()}
+              {activeLang?.value.toUpperCase()}
             </Text>
             <IconChevronDown size={14} stroke={1.5} />
           </Group>
@@ -31,7 +46,7 @@ const LanguagePicker = () => {
           <Menu.Item
             key={lang.value}
             leftSection={lang.flag}
-            onClick={() => i18n.changeLanguage(lang.value)}
+            onClick={() => setSelectedLanguage(lang.value)}
           >
             {lang.label}
           </Menu.Item>
