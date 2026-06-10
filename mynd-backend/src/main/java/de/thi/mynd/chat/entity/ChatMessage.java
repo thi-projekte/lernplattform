e2edccf568eaf60758d5ecceee11de7cf3e3ef95
Mathis Burger <handymathis@gmail.com>
2026-06-10 16:14:08 +0200

    feat: Added chat message schema

---
+++ mynd-backend/src/main/java/de/thi/mynd/chat/entity/ChatMessage.java
@@ -0,0 +1,19 @@
+package de.thi.mynd.chat.entity;
+
+import de.thi.mynd.common.entity.BaseEntityWithId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "chat_message")
+public class ChatMessage extends BaseEntityWithId {
+
+    @Column(nullable = false)
+    public UUID topicId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    public String message;
+}
