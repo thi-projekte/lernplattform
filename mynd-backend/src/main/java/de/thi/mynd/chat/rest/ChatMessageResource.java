78621cca7d378ba2d65e53189abc11c5561462a3
Mathis Burger <handymathis@gmail.com>
2026-06-10 20:57:17 +0200

    feat: Added chat message resource

---
+++ mynd-backend/src/main/java/de/thi/mynd/chat/rest/ChatMessageResource.java
@@ -0,0 +1,33 @@
+package de.thi.mynd.chat.rest;
+
+import de.thi.mynd.chat.dto.ChatMessageDto;
+import de.thi.mynd.chat.service.ChatMessageService;
+import de.thi.mynd.common.dto.PaginationDto;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.reactive.RestQuery;
+
+import java.util.UUID;
+
+@RolesAllowed("authorizedUser")
+@Tag(name = "Topic Chat")
+@SecurityRequirement(name = "keycloak")
+public final class ChatMessageResource {
+
+    @Inject
+    ChatMessageService chatMessageService;
+
+    @GET
+    @Path("/topics/{topicId}/chat")
+    @Operation(
+            summary = "Gets chat messages for a specific topic paginated",
+            description = "Gets chat messages for a specific topic paginated")
+    public PaginationDto<ChatMessageDto> getChatMessages(UUID topicId, @RestQuery int page, @RestQuery int pageSize) {
+        return chatMessageService.getMessages(topicId, page, pageSize);
+    }
+}
