665eeca4bb242d08667708f48614bc4f2f3dd382
Mathis Burger <handymathis@gmail.com>
2026-06-10 22:31:09 +0200

    feat: Implemented resource

---
+++ mynd-backend/src/main/java/de/thi/mynd/progressTracking/rest/TopicNoteResource.java
@@ -0,0 +1,40 @@
+package de.thi.mynd.progressTracking.rest;
+
+import de.thi.mynd.progressTracking.dto.TopicNoteDto;
+import de.thi.mynd.progressTracking.request.TopicNoteRequest;
+import de.thi.mynd.progressTracking.service.TopicNoteService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.util.UUID;
+
+@Path("/topic-notes")
+@RolesAllowed("authorizedUser")
+@Tag(name = "Topic Notes")
+@SecurityRequirement(name = "keycloak")
+public final class TopicNoteResource {
+
+    @Inject
+    TopicNoteService topicNoteService;
+
+    @GET
+    @Path("/{topicId}")
+    @Operation(summary = "Gets the personal topic notes for a specific topic")
+    public TopicNoteDto getTopicNote(UUID topicId) {
+        return topicNoteService.getTopicNoteForCurrentUser(topicId);
+    }
+
+    @PUT
+    @Path("/{topicId}")
+    @Operation(summary = "Updates the personal topic notes for a specific topic")
+    public TopicNoteDto updateTopicNote(UUID topicId, @Valid TopicNoteRequest request) {
+        return topicNoteService.updateTopicNoteForCurrentUser(topicId, request);
+    }
+}
