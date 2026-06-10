f52bf59c0c77e299d87c8e9fa240df64526f4f2c
Mathis Burger <handymathis@gmail.com>
2026-06-10 15:21:25 +0200

    feat: Added index card import Dtos

--- mynd-backend/src/main/java/de/thi/mynd/topic/dto/importer/ImportIndexCardDto.java
+++ mynd-backend/src/main/java/de/thi/mynd/topic/dto/importer/ImportIndexCardDto.java
@@ -3,13 +3,10 @@ package de.thi.mynd.topic.dto.importer;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @RegisterForReflection
 @Getter
-@Setter
-public final class ImportCategoryDto {
-  @NotBlank public String identifier;
-  @NotBlank public String title;
-  @NotBlank public String color;
+public class ImportIndexCardDto {
+    @NotBlank public String question;
+    @NotBlank public String answer;
 }
