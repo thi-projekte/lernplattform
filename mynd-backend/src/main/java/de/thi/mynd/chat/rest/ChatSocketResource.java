6abba5052f69667fd3fbb00737b992fa8302eef0
Mathis Burger <handymathis@gmail.com>
2026-06-16 23:24:41 +0200

    feat: Updated copyright

--- mynd-backend/src/main/java/de/thi/mynd/chat/rest/ChatSocketResource.java
+++ mynd-backend/src/main/java/de/thi/mynd/chat/rest/ChatSocketResource.java
@@ -1,10 +1,10 @@
 /**
  * This file is part of the MYnd application (de.thi.mynd).
  *
- *  Copyright (c) 2026 Projektgruppe MYnd B.Sc. WINF THI <mab8881@thi.de>
- *  
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
+ * Copyright (c) 2026 THI Projekte
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 package de.thi.mynd.chat.rest;
