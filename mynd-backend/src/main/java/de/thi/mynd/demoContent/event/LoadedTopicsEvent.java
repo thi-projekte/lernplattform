daa60c130348f731a2fdc35db95cf8a72ca2e5b2
Mathis Burger <handymathis@gmail.com>
2026-06-17 00:35:53 +0200

    chore: Fixed backend code formatting

--- mynd-backend/src/main/java/de/thi/mynd/demoContent/event/LoadedTopicsEvent.java
+++ mynd-backend/src/main/java/de/thi/mynd/demoContent/event/LoadedTopicsEvent.java
@@ -1,10 +1,10 @@
 /**
  * This file is part of the MYnd application (de.thi.mynd).
  *
- * Copyright (c) 2026 THI Projekte
+ * <p>Copyright (c) 2026 THI Projekte
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * <p>For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
  */
 package de.thi.mynd.demoContent.event;
 
